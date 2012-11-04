476c5b51b8533c9d1c927bac5b82ff373cf58b15
Radek Wierzbicki <rwierzbi@adobe.com>
2012-11-04 17:38:04 -0500

    Initial Commit

---
+++ bping/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  bping
+//
+//  Created by Radek Wierzbicki on 11/4/12.
+//  Copyright (c) 2012 Radek Wierzbicki. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
